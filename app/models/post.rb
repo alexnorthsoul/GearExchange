0a8270af4db9f411d69331017fc8c1dd577ddefa
Volodya Sveredyuk <sveredyuk@gmail.com>
2015-12-26 17:20:50 +0200

    Init likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,8 @@ class Post < ActiveRecord::Base
   acts_as_commentable
   acts_as_votable
 
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: "author_id"
   belongs_to :category
 
-  validates :title, :description, presence: true
+  validates :title, :description, :category, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,19 @@ Rails.application.routes.draw do
   #                   FRONT
   # ----------------------------------------------
 
-  devise_for :users, controllers: { omniauth_callbacks: 'auth_callbacks' }
   root 'home#index'
+
+  devise_for :users, controllers: { omniauth_callbacks: 'auth_callbacks' }
 
   match '/help',  to: 'home#help',  via: 'get'
   match '/about', to: 'home#about', via: 'get'
 
   resources :posts do
     resources :comments, only: %i(create destroy)
+    put :vote_up, :vote_down, on: :member
+
+    resources :comments, only: %i(create destroy) do
+    end
   end
 
   # ----------------------------------------------
